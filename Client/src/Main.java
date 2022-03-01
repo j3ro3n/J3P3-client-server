2630c9834d7671d58afd0ad739174b2f8d4190a5
Jeroen Buchholtz <wh.buchholtz@student.avans.nl>
2022-03-01 16:55:51 +0100

    Update ApplicatieProtocol.md Requirements.md, sidebar.md Added Infrastructuur.md

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
 }
+
+// TODO Client -> Jeroen
+// TODO Server -> Sunny
+// TODO Library -> Sven
+// TODO SSL certificaat maken doet Sven library
+// TODO accept maken zodat de server blijft draaien.
