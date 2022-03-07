3969922b0a89f7185c3efbeb3cb630ddd8074573
Jeroen Buchholtz <jeroen.buchholtz@politie.nl>
2022-03-07 23:13:01 +0100

    Tweede opzet client, server en service fileshare

--- Client/src/FileShareClient.java
+++ Client/src/FileShareClient.java
@@ -1,35 +1,116 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.Arrays;
+import java.util.*;
 
 public class FileShareClient {
-        public static int PORT=9443;
-        public static int BUFFER_SIZE=8*1024; // ik dacht 4*1024 = 4096kb te verdubbelen naar 8*1024 = 8192kb
-    public static void main(String[] args) throws Exception {
-        String fileName = null;
-        String ip = null;
-        try {
-            ip = args[0];
-            fileName = args[1];
-        } catch (Exception e) {
-            System.out.println("Voer de bestandsnaam in: "); // één commandline argument nodig
+    //public static int PORT = 9443;
+    //public static int BUFFER_SIZE = 8 * 1024; // ik dacht 4*1024 = 4096kb te verdubbelen naar 8*1024 = 8192kb
+
+    private static Socket sock;
+    private static String fileName;
+    private static BufferedReader stdin;
+    private static PrintStream os;
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            try {
+                sock = new Socket("127.0.0.1", 40000);
+                stdin = new BufferedReader(new InputStreamReader(System.in));
+            } catch (Exception e) {
+                System.err.println("Cannot connect to the server, try again later.");
+                System.exit(1);
+            }
+
+            os = new PrintStream(sock.getOutputStream());
+
+            try {
+                switch (Integer.parseInt(selectAction())) {
+                case 1:
+                    os.println("1");
+                    sendFile();
+                    continue;
+                case 2:
+                    os.println("2");
+                    System.out.println("Enter file name: ");
+                    fileName = stdin.readLine();
+                    os.println(fileName);
+                    receiveFile(fileName);
+                    continue;
+                case 3:
+                    sock.close();
+                    System.exit(1);
+            }
+        } catch(Exception e){
+            System.err.println("No valid input");
         }
 
-        File file = new File(fileName); // Hier het relatieve pad naar het bestand noteren.
-        Socket socket = new Socket(ip, PORT);
-
-        InputStream in = socket.getInputStream();
-        OutputStream out = new FileOutputStream("received.zip");
-
-        byte [] buffer = new byte[BUFFER_SIZE];
-        int bytesRead = 0;
-        while ((bytesRead = in.read(buffer)) > 0) {
-            out.write(buffer,0,bytesRead);
-        }
-
-        out.close(); // Hier sluit ik de output connectie
-        in.close();  // Hier sluit ik de input connectie
-        socket.close(); // Hier sluit ik de socket
-        System.out.println("FileShare gelukt!");
     }
+        }
+    public static String selectAction() throws IOException {
+        System.out.println("1. Send file.");
+        System.out.println("2. Receive file.");
+        System.out.println("3. Exit.");
+        System.out.println("\nMake selection: ");
+
+        return stdin.readLine();
+}
+    public static void sendFile() {
+        try {
+            System.out.println("Enter file name: ");
+            fileName = stdin.readLine();
+
+            File myFile = new File (fileName);
+            byte[] mybytearray = new byte[(int) myFile.length()];
+            if(!myFile.exists()) {
+                System.out.println("File does not exist...");
+                return;
+                }
+
+            FileInputStream fis = new FileInputStream(myFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            //bis.read(mybytearray, 0, mybytearray.length);
+
+            DataInputStream dis = new DataInputStream(bis);
+            dis.readFully(mybytearray, 0, mybytearray.length);
+
+            OutputStream os = sock.getOutputStream();
+
+            //Sending file name and file size to the server
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeUTF(myFile.getName());
+            dos.writeLong(mybytearray.length);
+            dos.write(mybytearray, 0, mybytearray.length);
+            dos.flush();
+            System.out.println("File "+fileName+" sent to Server.");
+        } catch (Exception e) {
+            System.err.println("Exception: "+e);
+        }
+    }
+
+    public static void receiveFile(String fileName) {
+        try {
+            int bytesRead;
+            InputStream in = sock.getInputStream();
+
+            DataInputStream clientData = new DataInputStream(in);
+
+            fileName = clientData.readUTF();
+            OutputStream output = new FileOutputStream(fileName);
+            long size = clientData.readLong();
+            byte[] buffer = new byte[1024];
+            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                output.write(buffer, 0, bytesRead);
+                size -= bytesRead;
+            }
+
+            output.close();
+            in.close();
+
+            System.out.println("File "+fileName+" received from Server.");
+        } catch (IOException ex) {
+            System.out.println("Exception: "+ex);
+        }
+
+    }
+
 }
---
+++ Server/src/FileShareServer.java
@@ -0,0 +1,35 @@
+import java.net.*;
+import java.io.*;
+
+public class FileShareServer {
+
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket = null;
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            serverSocket = new ServerSocket(40000);
+            System.out.println("Some one has started the FileSharing server! SushiShare Server started.");
+        } catch (Exception e) {
+            System.err.println("Port already in use.");
+            System.exit(1);
+        }
+
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection : " + clientSocket);
+
+                Thread t = new Thread(new ServiceShareClient(clientSocket));
+
+                t.start();
+
+            } catch (Exception e) {
+                System.err.println("Error in connection attempt.");
+            }
+        }
+    }
+}
+
